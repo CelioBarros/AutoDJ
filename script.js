a8790e297016ecbe2782d63ba2ff9343cf47d489
Joao Paulo Targino <joaotargino@gmail.com>
2015-01-24 19:33:55 -0300

    jquery local

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ $(document).ready(function () {
     $('#button').click(function(){
         var toAdd = $('input[name=checkListItem]').val();
         $(".list").append('<div class="item">' + toAdd + '</div>');
+        $("input[name=checkListItem], textarea").val("");
+
+
     });
 
     $(document).on('click', '.item', function(){
