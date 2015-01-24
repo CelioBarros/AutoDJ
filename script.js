3f67689e99c3a8fa5d714f6e471e294b6a5f8a45
Joao Paulo Targino <joaotargino@gmail.com>
2015-01-24 18:21:12 -0300

    interface web add/del + gerar lista

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $('#button').click(function(){
+        var toAdd = $('input[name=checkListItem]').val();
+        $(".list").append('<div class="item">' + toAdd + '</div>');
+    });
+
+    $(document).on('click', '.item', function(){
+        $(this).remove();
+    });
+
+    $('#gerarLista').click(function(){
+        alert("criando a lista perfeita");
+    });
+
+});
+
