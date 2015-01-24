6c456087dc67ea03d72a32b194952e043c87abf4
Joao Paulo Targino <joaotargino@gmail.com>
2015-01-24 20:02:48 -0300

    init comm js-flask

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 $(document).ready(function () {
+    var usuarios = [];
     $('#button').click(function(){
         var toAdd = $('input[name=checkListItem]').val();
         $(".list").append('<div class="item">' + toAdd + '</div>');
         $("input[name=checkListItem], textarea").val("");
+        usuarios.push(toAdd);
 
 
     });
 
-    $(document).on('click', '.item', function(){
-        $(this).remove();
-    });
+    //$(document).on('click', '.item', function(){
+    //    $(this).remove();
+    //});
 
     $('#gerarLista').click(function(){
-        alert("criando a lista perfeita");
+
+        $.ajax({
+            type: "POST",
+            url: "flask_server.py",
+            data: { param: usuarios}
+        }).done(function (o){
+            alert("criando a lista perfeita");
+        });
+
+        for(var i = 0; i < usuarios.length ; i++) {
+            console.log(usuarios[i]);
+        }
     });
 
 });
